1469913a3af1a560eefacb44fa374919e696190f
Dmitri Botcharnikov <dmitry.b@samsung.com>
2016-06-28 13:42:28 +0300

    Update after review

--- src/System.Diagnostics.Debug.SymbolReader/src/System/Diagnostics/Debug/SymbolReader/SymbolReader.cs
+++ src/System.Diagnostics.Debug.SymbolReader/src/System/Diagnostics/Debug/SymbolReader/SymbolReader.cs
@@ -23,8 +23,10 @@ namespace System.Diagnostics.Debug.SymbolReader
         public static bool LoadSymbolsForModule(string assemblyFileName)
         {
             MetadataReader peReader, pdbReader;
-            
-            return GetReaders(assemblyFileName, out peReader, out pdbReader);
+            bool found = GetReaders(assemblyFileName, out peReader, out pdbReader);
+            peReader = null;
+            pdbReader = null;
+            return found;
         }
     
         /// <summary>
@@ -40,26 +42,34 @@ namespace System.Diagnostics.Debug.SymbolReader
             MetadataReader peReader, pdbReader;
             methToken = 0;
             ilOffset = 0;
-            if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
-                return;
             
-            foreach (MethodDefinitionHandle methodDefHandle in peReader.MethodDefinitions)
-            {
-                MethodDebugInformation methodDebugInfo = pdbReader.GetMethodDebugInformation(methodDefHandle);
-                SequencePointCollection sequencePoints = methodDebugInfo.GetSequencePoints();
-                foreach (SequencePoint point in sequencePoints)
+            try {
+                if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
+                    return;
+                
+                foreach (MethodDefinitionHandle methodDefHandle in peReader.MethodDefinitions)
                 {
-                    string sourceName = pdbReader.GetString(pdbReader.GetDocument(point.Document).Name);
-                    if (Path.GetFileName(sourceName) == Path.GetFileName(fileName) && point.StartLine == lineNumber)
+                    MethodDebugInformation methodDebugInfo = pdbReader.GetMethodDebugInformation(methodDefHandle);
+                    SequencePointCollection sequencePoints = methodDebugInfo.GetSequencePoints();
+                    foreach (SequencePoint point in sequencePoints)
                     {
-                        methToken = MetadataTokens.GetToken(peReader, methodDefHandle);
-                        ilOffset = point.Offset;
-                        return;
+                        string sourceName = pdbReader.GetString(pdbReader.GetDocument(point.Document).Name);
+                        if (Path.GetFileName(sourceName) == Path.GetFileName(fileName) && point.StartLine == lineNumber)
+                        {
+                            methToken = MetadataTokens.GetToken(peReader, methodDefHandle);
+                            ilOffset = point.Offset;
+                            return;
+                        }
                     }
                 }
-                
+            }
+            finally
+            {
+                peReader = null;
+                pdbReader = null;
             }
         }
+
         /// <summary>
         /// Returns source line number and source file name for given IL offset and method token.
         /// </summary>
@@ -67,38 +77,73 @@ namespace System.Diagnostics.Debug.SymbolReader
         /// <param name="methToken">method token</param>
         /// <param name="ilOffset">IL offset</param>
         /// <param name="lineNumber">source line number return</param>
-        /// <param name="fileName">source file name</param>
-        public static int GetLineByILOffset(string assemblyFileName, int methodToken, int ilOffset, out int lineNumber, out IntPtr fileName)
+        /// <param name="fileName">source file name return</param>
+        /// <returns> true if information is available</returns>
+        public static bool GetLineByILOffset(string assemblyFileName, int methodToken, int ilOffset, out int lineNumber, out IntPtr fileName)
+        {
+            lineNumber = 0;
+            fileName = IntPtr.Zero;
+
+            string sourceFileName = null;
+            
+            if (!GetSourceLineByILOffset(assemblyFileName, methodToken, ilOffset, out lineNumber, out sourceFileName)
+                return false;
+            
+            fileName = Marshal.StringToBSTR(sourceFileName);
+            sourceFileName = null;
+            return true;
+        }
+        
+        /// <summary>
+        /// Helper method to return source line number and source file name for given IL offset and method token.
+        /// </summary>
+        /// <param name="assemblyFileName">file name of the assembly</param>
+        /// <param name="methToken">method token</param>
+        /// <param name="ilOffset">IL offset</param>
+        /// <param name="lineNumber">source line number return</param>
+        /// <param name="fileName">source file name return</param>
+        /// <returns> true if information is available</returns>
+        private static bool GetSourceLineByILOffset(string assemblyFileName, int methodToken, int ilOffset, out int lineNumber, out string fileName)
         {
             MetadataReader peReader, pdbReader;
             lineNumber = 0;
-            fileName = IntPtr.Zero;
-            if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
-                return -1;
-            Handle handle = MetadataTokens.Handle(methodToken);
-            if (handle.Kind != HandleKind.MethodDefinition)
-                return -1;
-
-            MethodDebugInformationHandle methodDebugHandle = ((MethodDefinitionHandle)handle).ToDebugInformationHandle();
-            MethodDebugInformation methodDebugInfo = pdbReader.GetMethodDebugInformation(methodDebugHandle);
-            SequencePointCollection sequencePoints = methodDebugInfo.GetSequencePoints();
-
-            SequencePoint nearestPoint = sequencePoints.GetEnumerator().Current;
-            foreach (SequencePoint point in sequencePoints)
+            fileName = null;
+            
+            try
             {
-                if (point.Offset <= ilOffset)
-                    nearestPoint = point;
-                else
+                if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
+                    return false;
+                Handle handle = MetadataTokens.Handle(methodToken);
+                if (handle.Kind != HandleKind.MethodDefinition)
+                    return false;
+
+                MethodDebugInformationHandle methodDebugHandle = ((MethodDefinitionHandle)handle).ToDebugInformationHandle();
+                MethodDebugInformation methodDebugInfo = pdbReader.GetMethodDebugInformation(methodDebugHandle);
+                SequencePointCollection sequencePoints = methodDebugInfo.GetSequencePoints();
+
+                SequencePoint nearestPoint = sequencePoints.GetEnumerator().Current;
+                foreach (SequencePoint point in sequencePoints)
                 {
-                    if (nearestPoint.StartLine == 0)
-                        return -1;
-                    lineNumber = nearestPoint.StartLine;
-                    fileName = Marshal.StringToBSTR(pdbReader.GetString(pdbReader.GetDocument(nearestPoint.Document).Name));
-                    return 0;
+                    if (point.Offset <= ilOffset)
+                        nearestPoint = point;
+                    else
+                    {
+                        if (nearestPoint.StartLine == 0)
+                            return false;
+                        lineNumber = nearestPoint.StartLine;
+                        fileName = pdbReader.GetString(pdbReader.GetDocument(nearestPoint.Document).Name);
+                        return true;
+                    }
                 }
+                return false;
             }
-            return -1;
+            finally
+            {
+                peReader = null;
+                pdbReader = null;
+            }
         }
+        
         
         /// <summary>
         /// Returns local variable name for given local index and IL offset.
@@ -110,35 +155,64 @@ namespace System.Diagnostics.Debug.SymbolReader
         /// <returns>true if name has been found</returns>
         public static bool GetLocalVariableName(string assemblyFileName, int methodToken, int localIndex, out IntPtr localVarName)
         {
-            MetadataReader peReader, pdbReader;
             localVarName = IntPtr.Zero;
 
-            if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
+            string localVar = null;
+            if (!GetLocalVariableByIndex(assemblyFileName, methodToken, localIndex, out localVar))
                 return false;
+            
+            localVarName = Marshal.StringToBSTR(localVar);
+            localVar = null;
+            return true;
+        }
+    
+        /// <summary>
+        /// Helper method to return local variable name for given local index and IL offset.
+        /// </summary>
+        /// <param name="assemblyFileName">file name of the assembly</param>
+        /// <param name="methodToken">method token</param>
+        /// <param name="localIndex">local variable index</param>
+        /// <param name="localVarName">local variable name return</param>
+        /// <returns>true if name has been found</returns>
+        public static bool GetLocalVariableByIndex(string assemblyFileName, int methodToken, int localIndex, out string localVarName)
+        {
+            MetadataReader peReader, pdbReader;
+            localVarName = null;
 
-            Handle handle = MetadataTokens.Handle(methodToken);
-            if (handle.Kind != HandleKind.MethodDefinition)
-                return false;
-
-            MethodDebugInformationHandle methodDebugHandle = ((MethodDefinitionHandle)handle).ToDebugInformationHandle();
-            LocalScopeHandleCollection localScopes = pdbReader.GetLocalScopes(methodDebugHandle);
-            foreach (LocalScopeHandle scopeHandle in localScopes)
+            try
             {
-                LocalScope scope = pdbReader.GetLocalScope(scopeHandle);
-                LocalVariableHandleCollection localVars = scope.GetLocalVariables();
-                foreach (LocalVariableHandle varHandle in localVars)
+                if (!GetReaders(assemblyFileName, out peReader, out pdbReader))
+                    return false;
+                
+                Handle handle = MetadataTokens.Handle(methodToken);
+                if (handle.Kind != HandleKind.MethodDefinition)
+                    return false;
+
+                MethodDebugInformationHandle methodDebugHandle = ((MethodDefinitionHandle)handle).ToDebugInformationHandle();
+                LocalScopeHandleCollection localScopes = pdbReader.GetLocalScopes(methodDebugHandle);
+                foreach (LocalScopeHandle scopeHandle in localScopes)
                 {
-                    LocalVariable localVar = pdbReader.GetLocalVariable(varHandle);
-                    if (localVar.Index == localIndex)
+                    LocalScope scope = pdbReader.GetLocalScope(scopeHandle);
+                    LocalVariableHandleCollection localVars = scope.GetLocalVariables();
+                    foreach (LocalVariableHandle varHandle in localVars)
                     {
-                        if (localVar.Attributes == LocalVariableAttributes.DebuggerHidden)
-                            return false;
-                        localVarName = Marshal.StringToBSTR(pdbReader.GetString(localVar.Name));
-                        return true;
+                        LocalVariable localVar = pdbReader.GetLocalVariable(varHandle);
+                        if (localVar.Index == localIndex)
+                        {
+                            if (localVar.Attributes == LocalVariableAttributes.DebuggerHidden)
+                                return false;
+                            localVarName = pdbReader.GetString(localVar.Name);
+                            return true;
+                        }
                     }
                 }
+                return false;
             }
-            return false;
+            finally
+            {
+                peReader = null;
+                pdbReader = null;
+            }
         }
     
         /// <summary>
